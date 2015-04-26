99809474454e493c02858ba33ff8940a0462013d
qzwpq <qzwpqpwq@gmail.com>
2015-04-26 18:06:02 +0900

    First commit.

---
+++ client/client.js
@@ -0,0 +1,178 @@
+Meteor.subscribe("tags");
+var MY_TWEETS_LIST_NUM=10;
+
+var ulRotate=function(targetTag,newLi){
+	var $ul=$(targetTag);
+	$ul.prepend("<li>"+newLi+"</li>");
+	if($ul.children().length>=MY_TWEETS_LIST_NUM){
+		$ul.children(":last-child").remove();
+	}
+};
+
+var sanitize=function(text){
+	return $("<div>").text(text).html();
+};
+
+Template.tweetArea.events({
+	"submit #newTweet": function(event) {
+		var text = event.target.text.value;
+		text = text.trim();
+		if (!text) {
+			event.target.text.value = "";
+			$("#newTweetText").focus();
+			return false;
+		}
+		var station = $("#station").val();
+		var program = $("#program").val();
+		var tagArray=[station,program];
+		Meteor.call("postTweet", text,tagArray, function(err, res) {
+			if (err) console.log(err);
+			var result = !err ? res.data.text : text;
+			var isSucceeded = !err ? "ok" : "remove";
+			isSucceeded='<span class="glyphicon glyphicon-'+isSucceeded+'"></span> ';
+			result=sanitize(result);
+			ulRotate("#myTweetsList",isSucceeded+result);
+		});
+		event.target.text.value = "";
+		$("#newTweetText").focus();
+		return false;
+	}
+});
+
+Template.configTagModal.events({
+	"submit #addStation": function(event) {
+		var station = event.target.text.value;
+		Meteor.call("addStation",station);
+		event.target.text.value = "";
+		return false;
+	},
+	"submit #addProgram": function(event) {
+		var program = event.target.text.value;
+		var station=$(event.target).find(":selected").val();
+		Meteor.call("addProgram",program,station);
+		event.target.text.value = "";
+		return false;
+	},
+	"submit #delStation":function(event){
+		var station=$(event.target).find(":selected").val();
+		Meteor.call("delStation",station);
+		return false;
+	},
+	"submit #delProgram":function(){
+		var program=$("#unwantedProgram").val();
+		var station=$("#unwantedStation").val();
+		Meteor.call("delProgram",program,station);
+		return false;
+	}
+});
+
+Template.getTagModal.events({
+	"submit #getTag":function(){
+		$("#tagSuggestion").empty();
+		$("#tagSuggestionStatus").text("Loading...");
+		$(".timeline").each(function(){
+			$(this).prop("disabled",true);
+		});
+		var n=200;
+		Meteor.call("getHomeTimeline",n,function(err,res){
+			if(err)console.log(err);
+			$("#tagSuggestionStatus").empty();
+			var tagPool=[];
+			for (var i = res.data.length - 1; i >= 0; i--) {
+				for (var j = res.data[i].entities.hashtags.length - 1; j >= 0; j--) {
+					var tag=res.data[i].entities.hashtags[j].text;
+					tag=sanitize(tag);
+					tagPool.push(tag);
+				}
+			}
+			if(tagPool.length===0){
+				$("#tagSuggestionStatus").text("There are no tags in your timeline.");
+				return false;
+			}
+			var result=_.chain(tagPool)
+			.countBy(function(n){return n;})
+			.pairs()
+			.sortBy(function(a){return a[1];})
+			.value();
+			/* -> [ [name, count_i], [name, count_j] .....]
+			count_i <= count_j if i <j */
+			var tagCount=result[result.length-1][1];
+			var html='<optgroup label="'+tagCount+'回">';
+			for (var i = result.length - 1; i >= 0; i--) {
+				if(tagCount!==result[i][1]){
+					tagCount=result[i][1];
+					html+='</optgroup><optgroup label="'+tagCount+'回">';
+				}
+				html+='<option value="'+result[i][0]+'">'+result[i][0]+"</option>";
+			}
+			html+="</optgroup>";
+			$("#tagSuggestion").append(html);
+			$(".timeline").each(function(){
+				$(this).prop("disabled",function(i, v) { return !v; });
+			});
+			$("#tagSuggestion").scrollTop(0);
+			$("#tagSuggestionStatus").empty();
+		});
+		return false;
+	},
+	"submit #addStationFromSuggestion":function(event){
+		$("#tagSuggestion").find(":selected").each(function(){
+			var station=$(this).val();
+			Meteor.call("addStation",station);
+			$(this).remove();
+		});
+		$("optgroup").each(function(){
+			if($(this).children().length===0)$(this).remove();
+		});
+		return false;
+	},
+	"submit #addProgramFromSuggestion":function(event){
+		var station=$(".getTag #targetStation").val();
+		$("#tagSuggestion").find(":selected").each(function(){
+			var program=$(this).val();
+			Meteor.call("addProgram",program,station);
+			$(this).remove();
+		});
+		$("optgroup").each(function(){
+			if($(this).children().length===0)$(this).remove();
+		});
+		return false;
+	}
+});
+$(function($) {
+	//Ctrl+Enter
+	$(window).keydown(function(e) {
+		if (e.ctrlKey) {
+			if (e.keyCode === 13) {
+				$("#newTweet").submit();
+				return false;
+			}
+		}
+	});
+	$('button[data-target="#footerContent"]').click(function(){
+		$(this).text(function(i,old){
+			return old==="隠す"?"開く":"隠す";
+		});
+	});
+});
+
+Template.stationList.helpers({
+	stations: function(){
+		if(!Meteor.user()) return [];
+		var tmp=Object.keys(Tags.findOne().stations);
+		tmp=_.without(tmp,"blank_");
+		return tmp;
+	}
+});
+
+Template.programList.helpers({
+	programs: function(){
+		if(!Meteor.user()) return [];
+		var station=Tags.findOne().selectedStation;
+		return Tags.findOne().stations[station].programs;
+	}
+});
+
+Template.configTagModalBody.helpers({
+	defaultStations:["tokyomx","tvk","tvtokyo","fujitv","tbs","tvasahi","ntv","etv","nhk"]
+});
---
+++ server/server.js
@@ -0,0 +1,117 @@
+var config=Assets.getText("config.json");
+config=JSON.parse(config);
+
+Meteor.startup(function(){
+	Accounts.loginServiceConfiguration.remove({service:"twitter"});
+	Accounts.loginServiceConfiguration.insert({
+		service:"twitter",
+		consumerKey:config.consumerKey,
+		secret:config.secret
+	});
+});
+
+TwitterApi.prototype.homeTimeline = function(param) {
+	return this.get('statuses/home_timeline.json',param);
+};
+
+var twitter=new TwitterApi();
+
+function validateString(s) {
+	return ((typeof s)==="string")&&(s!=="")&&(s.match(/\./)===null);
+}
+
+function confirmStation (stationName,userId) {
+	var selector={};
+	selector["stations."+stationName]={$exists:true};
+	selector["owner"]=userId;
+	return !!Tags.find(selector).count();
+}
+
+function confirmProgram (program,stationName,userId) {
+	var selector={};
+	selector["stations."+stationName+".programs"]=program;
+	selector["owner"]=userId;
+	return !!Tags.find(selector).count();
+}
+
+Meteor.methods({
+	postTweet:function(text,tagArray){
+		if(Meteor.user()){
+			_.chain(tagArray)
+			.filter(function(a){return (validateString(a))&&(a!=="blank_");})
+			.map(function(a){text=text+" #"+a;});
+			text=text.trim();
+			return twitter.postTweet(text);
+		}
+	},
+	addStation:function(stationName){
+		if(validateString(stationName) && Meteor.user()){
+			//confirm the new station name does not exist
+			if(!confirmStation(stationName,this.userId)){
+				var selector={};
+				selector["stations."+stationName+".createdAt"]=new Date();
+				selector["stations."+stationName+".programs"]=[];
+				Tags.update({owner:this.userId},{$set:selector});
+			}
+		}
+	},
+	addProgram:function(program,stationName){
+		if(Meteor.user()&&validateString(program)&&validateString(stationName)){
+			if(confirmStation(stationName,this.userId)&&(!confirmProgram(program,stationName,this.userId))){
+				var selector={};
+				selector["stations."+stationName+".programs"]=program;
+				Tags.update({owner:this.userId},{$push:selector});
+			}
+		}
+	},
+	delStation:function(stationName){
+		if(Meteor.user()&&stationName!=="blank_"){
+			var selector={};
+			selector["stations."+stationName]="";
+			Tags.update({owner:this.userId},{$unset:selector,$set:{selectedStation:"blank_"}});
+		}
+	},
+	delProgram:function(program,stationName){
+		if(Meteor.user()){
+			var selector={};
+			selector["stations."+stationName+".programs"]=program;
+			Tags.update({owner:this.userId},{$pull:selector});
+		}
+	},
+	changeStation:function(stationName){
+		if(Meteor.user()&&confirmStation(stationName,this.userId)){
+			Tags.update({owner:this.userId},{$set:{selectedStation:stationName}});
+		}
+	},
+	getHomeTimeline:function(n){
+		if(Meteor.user()&&n>0&&n<=200){
+			return twitter.homeTimeline({count:n});
+		}
+	}
+});
+
+Meteor.publish("tags",function(){
+	if(Tags.find({owner:this.userId}).count()===0){
+		Tags.insert({
+			owner:this.userId,
+			stations:{
+				blank_:{
+					createdAt:new Date(),
+					programs:[]
+				}
+			},
+			selectedStation:"blank_"
+		});
+	}
+	else {
+		Tags.update({
+				owner:this.userId
+			},
+			{
+				$set:{
+					selectedStation:"blank_"
+			}
+		});
+	}
+	return Tags.find({owner:this.userId});
+});
